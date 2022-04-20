66a571ff91ef1a32aa8200f661dbdb4653ec06dd
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-20 11:49:57 -0500

    Adding base modules
    Adding base modules.

---
+++ src/main/java/com/revature/user/service/UserService.java
@@ -0,0 +1,25 @@
+package com.revature.user.service;
+
+import com.revature.user.model.User;
+import com.revature.user.repository.UserRepository;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User createNewUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findUserById(Integer id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found."));
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found."));
+    }
+}
