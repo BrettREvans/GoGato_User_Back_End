7a779dd19038274fcf4e5241993d793ebc2edb3a
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-20 12:04:46 -0500

    Update UserController.java
    Just reformatting.

--- src/main/java/com/revature/user/controller/UserController.java
+++ src/main/java/com/revature/user/controller/UserController.java
@@ -10,23 +10,24 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping
-    public User createNewUser(@RequestBody User user){
+    public User createNewUser(@RequestBody User user) {
         return userService.createNewUser(user);
     }
 
+    // /users/mickey
     @GetMapping("/{identifier}")
-    public User findUserByIdOrUsername(@PathVariable String identifier){
-
-        try{
+    public User findUserByIdOrUsername(@PathVariable String identifier) {
+        try {
             Integer id = Integer.parseInt(identifier);
 
             return userService.findUserById(id);
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
+            // we know that the identifier string is not a number, so we can use it as a username
             return userService.findUserByUsername(identifier);
         }
     }
