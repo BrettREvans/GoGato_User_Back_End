6839557bbf9eab0ef49180dca2cc4aa7fe4a506a
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-25 09:37:37 -0500

    Created ProfileController
    Moved updateAboutMe into ProfileController from UserController.

---
+++ src/main/java/com/revature/user/controller/ProfileController.java
@@ -0,0 +1,37 @@
+package com.revature.user.controller;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.revature.user.model.User;
+import com.revature.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/profiles")
+public class ProfileController {
+
+    @Autowired
+    UserService userService;
+
+    /**
+     * Update a User's profile information.
+     *
+     * @param aboutMeObject New user information
+     */
+    @PutMapping("/profile/{identifier}")
+    public void updateAboutMe(@PathVariable String identifier, @RequestBody ObjectNode aboutMeObject) {
+
+        String aboutMeText = aboutMeObject.get("aboutMe").asText();
+
+        User userToUpdate = new User();
+        // Get the user to update by their ID or username
+        try {
+            Integer id = Integer.parseInt(identifier);
+            userToUpdate = userService.findUserById(id);
+        } catch (NumberFormatException e) {
+            userToUpdate = userService.findByUsername(identifier);
+        }
+
+        userService.updateAboutMe(userToUpdate, aboutMeText);
+    }
+}
