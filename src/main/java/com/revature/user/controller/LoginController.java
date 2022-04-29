cc02b37c3b7bd065f8b0c213c334be0496dad7f3
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-29 14:22:34 -0500

    Changed author tags to lowercase

--- src/main/java/com/revature/user/controller/LoginController.java
+++ src/main/java/com/revature/user/controller/LoginController.java
@@ -12,36 +12,38 @@ public class LoginController {
     private final UserService userService;
 
     /**
-     * @Author Tyler, Boualem, Jason
      * Constructor -> Injects needed dependencies
+     *
      * @param userService UserService dependency
+     * @author Tyler, Boualem, Jason
      */
     public LoginController(UserService userService) {
         this.userService = userService;
     }
 
     /**
-     * @Author Tyler, Boualem, Jason
      * Method -> Checks a User that is logging in
+     *
      * @param user The info to be checked
      * @return The User after validation
+     * @author Tyler, Boualem, Jason
      */
     @PostMapping
     public Integer checkLogin(@RequestBody User user) {
         User emptyUser = new User();
         if (user != null) {
             User dbUser = userService.findByUsername(user.getUsername());
-
-            if (dbUser != null) // Check if User exists
-            {
-                if (userService.comparePassword(user.getPassword(), dbUser.getPassword()))  // Check password
-                {
-                    return dbUser.getId();  // User credentials match
+            // Check if User exists
+            if (dbUser != null) {
+                // Check password
+                if (userService.comparePassword(user.getPassword(), dbUser.getPassword())) {
+                    // User credentials match
+                    return dbUser.getId();
                 }
             }
         }
-
-        return  0;    // Returns the User, has no ID if User had wrong password or doesn't exist
+        // Returns the User, has no ID if User had wrong password or doesn't exist
+        return 0;
     }
 
 }
